6c2ed702875c4ad8d3859ff233c34e37e6f4562c
Jose Valencia <jose.29vabe@gmail.com>
2024-05-26 02:31:52 -0500

    logs de back para revisar post

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import tensorflow as tf
 import numpy as np
 import base64
 from io import BytesIO
+import sys
 from PIL import Image
+import logging
 
 app = Flask(__name__)
 
 # Cargar el modelo entrenado
 model = tf.keras.models.load_model('model_lenet.h5')
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 def preprocess_image(image):
     image = image.convert('L').resize((28, 28))
@@ -25,10 +29,14 @@ def index():
 def predict():
     data = request.get_json()
     image_data = data['image'].split(',')[1]
+    logging.debug(f'image_data = {image_data}')
     image = Image.open(BytesIO(base64.b64decode(image_data)))
+    logging.debug('Tamaño de la imagen: %s', image.size)
     processed_image = preprocess_image(image)
+    logging.debug('Forma de la imagen procesada: %s', processed_image.shape)
     prediction = model.predict(processed_image)
     predicted_number = np.argmax(prediction)
+    logging.debug('Predicción: %s', predicted_number)
     return jsonify({'number': int(predicted_number)})
 
 if __name__ == '__main__':
